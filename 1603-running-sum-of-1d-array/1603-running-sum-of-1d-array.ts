5ebf07cd5d7940132e759fb627207ffd9a6d2d96
HeraclesPath <18598199+HeraclesPath@users.noreply.github.com>
2025-03-08 14:52:34 -0500

    Time: 0 ms (100%), Space: 58.4 MB (6.26%) - LeetHub

---
+++ 1603-running-sum-of-1d-array/1603-running-sum-of-1d-array.ts
@@ -0,0 +1,9 @@
+function runningSum(nums: number[]): number[] {
+    let result: number[] = [];
+    let sum = 0;
+    for (let i = 0; i < nums.length; i++) {
+        sum += nums[i];
+        result.push(sum);
+    }
+    return result;
+};
