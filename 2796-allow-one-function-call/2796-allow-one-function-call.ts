855221522d949fd18dc82ead0d74e6364c62ccc1
HeraclesPath <18598199+HeraclesPath@users.noreply.github.com>
2025-03-14 06:26:18 -0400

    Time: 38 ms (98.14%), Space: 54.1 MB (32.09%) - LeetHub

---
+++ 2796-allow-one-function-call/2796-allow-one-function-call.ts
@@ -0,0 +1,23 @@
+type JSONValue = null | boolean | number | string | JSONValue[] | { [key: string]: JSONValue };
+type OnceFn = (...args: JSONValue[]) => JSONValue | undefined
+
+function once(fn: Function): OnceFn {
+    let called = false;
+    let result;
+    return function (...args) {
+        if (!called) {
+            result = fn(...args);
+            called = true;
+            return result;
+        }
+        return undefined;
+    };
+}
+
+/**
+ * let fn = (a,b,c) => (a + b + c)
+ * let onceFn = once(fn)
+ *
+ * onceFn(1,2,3); // 6
+ * onceFn(2,3,6); // returns undefined without calling fn
+ */
