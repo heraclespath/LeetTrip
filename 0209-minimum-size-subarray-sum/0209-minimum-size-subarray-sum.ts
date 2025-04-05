493e7e23f0e03d7e57204fa670632e0dec582b65
HeraclesPath <18598199+HeraclesPath@users.noreply.github.com>
2025-04-05 16:25:19 -0400

    Time: 3 ms (70.03%), Space: 61.3 MB (42.28%) - LeetHub

---
+++ 0209-minimum-size-subarray-sum/0209-minimum-size-subarray-sum.ts
@@ -0,0 +1,16 @@
+function minSubArrayLen(target: number, nums: number[]): number {
+  let left = 0;
+  let sum = 0;
+  let minLen = Infinity;
+
+  for (let right = 0; right < nums.length; right++) {
+    sum += nums[right];
+
+    while (sum >= target) {
+      minLen = Math.min(minLen, right - left + 1);
+      sum -= nums[left++];
+    }
+  }
+
+  return minLen === Infinity ? 0 : minLen;
+}
