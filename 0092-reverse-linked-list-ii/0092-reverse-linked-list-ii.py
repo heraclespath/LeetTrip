9c1fe68ecf2fdb60c0ccbd029df2b90f0808b126
HeraclesPath <18598199+HeraclesPath@users.noreply.github.com>
2025-04-17 17:20:49 -0700

    Time: 0 ms (100%), Space: 12.8 MB (5.6%) - LeetHub

---
+++ 0092-reverse-linked-list-ii/0092-reverse-linked-list-ii.py
@@ -0,0 +1,28 @@
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution(object):
+    def reverseBetween(self, head, left, right):
+        """
+        :type head: Optional[ListNode]
+        :type left: int
+        :type right: int
+        :rtype: Optional[ListNode]
+        """
+        if head.next is None or left == right:
+            return head
+        dummy = ListNode(0, head)
+        pre = dummy
+        for _ in range(left - 1):
+            pre = pre.next
+        p, q = pre, pre.next
+        cur = q
+        for _ in range(right - left + 1):
+            t = cur.next
+            cur.next = pre
+            pre, cur = cur, t
+        p.next = pre
+        q.next = cur
+        return dummy.next
