5a13b644fd810a83a3762cbd21cb538ab57a76e5
HeraclesPath <18598199+HeraclesPath@users.noreply.github.com>
2025-05-03 16:25:18 -0400

    Time: 0 ms (100%), Space: 9.1 MB (28.3%) - LeetHub

---
+++ 0275-h-index-ii/0275-h-index-ii.go
@@ -0,0 +1,15 @@
+func hIndex(citations []int) int {
+    n := len(citations)
+    left, right := 0, n-1
+
+    for left <= right {
+        mid := left + (right-left)/2
+        if citations[mid] >= n - mid {
+            right = mid - 1
+        } else {
+            left = mid + 1
+        }
+    }
+
+    return n - left
+}
