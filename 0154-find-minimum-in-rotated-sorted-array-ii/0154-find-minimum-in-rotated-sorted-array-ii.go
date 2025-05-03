dce700beca0d4d613382457f92c6f29f84dddd5f
HeraclesPath <18598199+HeraclesPath@users.noreply.github.com>
2025-05-03 16:23:17 -0400

    Time: 0 ms (100%), Space: 4.9 MB (33.33%) - LeetHub

---
+++ 0154-find-minimum-in-rotated-sorted-array-ii/0154-find-minimum-in-rotated-sorted-array-ii.go
@@ -0,0 +1,17 @@
+func findMin(nums []int) int {
+    left, right := 0, len(nums)-1
+
+    for left < right {
+        mid := left + (right-left)/2
+
+        if nums[mid] < nums[right] {
+            right = mid
+        } else if nums[mid] > nums[right] {
+            left = mid + 1
+        } else {
+            right--
+        }
+    }
+
+    return nums[left]
+}
