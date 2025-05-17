b95e28645ac9c026e4a24b8d92b3a8c74bfee7fa
HeraclesPath <18598199+HeraclesPath@users.noreply.github.com>
2025-05-17 12:56:10 -0400

    Time: 0 ms (100%), Space: 9.7 MB (7.28%) - LeetHub

---
+++ 0283-move-zeroes/0283-move-zeroes.go
@@ -0,0 +1,15 @@
+func moveZeroes(nums []int)  {
+    insertPos := 0
+
+    for _, num := range nums {
+        if num != 0 {
+            nums[insertPos] = num
+            insertPos++
+        }
+    }
+
+    for insertPos < len(nums) {
+        nums[insertPos] = 0
+        insertPos++
+    }
+}
